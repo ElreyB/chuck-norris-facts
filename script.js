5496e906d019d0b8024a15dbb56beeabada611b1
Elrey Belmonti <elbelmonti@gmail.com>
2020-01-02 07:33:01 -0800

    add text from api and style html

--- script.js
+++ script.js
@@ -1,25 +1,67 @@
 $(document).ready(function() {
-  console.log("I am ready!");
   var queryURL =
     "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=chuck%norris";
-
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    // var random = Math.floor(Math.random() * response.data.length) + 1;
-    console.log(response.data);
-    const results = response.data;
-    results.forEach((gif, i) => {
-      const gifUrl = gif.images.original.url;
-      const carouselItemEl =
-        i === 0
-          ? $(`<div class="carousel-item active">`)
-          : $(`<div class="carousel-item">`);
-      const img = $("<img>");
+    var results = response.data;
+
+    for (var i = 0; i < results.length; i++) {
+      var gifUrl = results[i].images.original.url;
+
+      var carouselItemEl = createCarouselItem(i);
+
+      var img = $("<img>");
       img.attr({ src: gifUrl, class: "d-block w-100", alt: "gif" });
+
       carouselItemEl.append(img);
+
+      chuckDevJoke(carouselItemEl);
+
       $(".carousel-inner").append(carouselItemEl);
-    });
+    }
+    // results.forEach((gif, i) => {
+    //   var gifUrl = gif.images.original.url;
+    //   var carouselItemEl = createCarouselItem(i);
+    //   var img = $("<img>");
+    //   img.attr({ src: gifUrl, class: "d-block w-100", alt: "gif" });
+    //   carouselItemEl.append(img);
+    //   chuckDevJoke(carouselItemEl);
+    //   $(".carousel-inner").append(carouselItemEl);
+    // });
   });
+
+  function createCarouselItem(index) {
+    var carouselItemEl =
+      index === 0
+        ? $("<div class='carousel-item active' data-interval='5000'>")
+        : $("<div class=carousel-item data-interval='5000'>");
+    return carouselItemEl;
+  }
+
+  function chuckDevJoke(element) {
+    var queryURL =
+      "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/categories";
+    var settings = {
+      async: true,
+      crossDomain: true,
+      url:
+        "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random?category=dev",
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com",
+        "x-rapidapi-key": "26fcba3080msh3a2259b034b4c3dp1f9e01jsn7b1d1f231b17",
+        accept: "application/json"
+      }
+    };
+    $.ajax(settings).then(function(response) {
+      var captionEl = $("<div></div>");
+      captionEl.addClass("carousel-caption d-none d-md-block");
+      var text = response.value;
+      var textEl = $(`<h2>${text}</h2>`);
+      captionEl.append(textEl);
+      element.append(captionEl);
+    });
+  }
 });
