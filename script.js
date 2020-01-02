1fec9165ca678fc8a5a7c4bab18488699cad7415
Elrey Belmonti <elbelmonti@gmail.com>
2020-01-01 18:37:27 -0800

    add gifs and center carousel

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-// var queryURL =
-// "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=chuck%norris";
+$(document).ready(function() {
+  console.log("I am ready!");
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=chuck%norris";
 
-// $.ajax({
-// url: queryURL,
-// method: "GET"
-// }).then(function(response) {
-// var random = Math.floor(Math.random() * response.data.length) + 1;
-// console.log(response.data[random].images.original.url);
-// const gif = response.data[random].images.original.url;
-// const img = $("<img>");
-// img.attr("src", gif);
-// $("#gifs").append(img);
-// });
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    // var random = Math.floor(Math.random() * response.data.length) + 1;
+    console.log(response.data);
+    const results = response.data;
+    results.forEach((gif, i) => {
+      const gifUrl = gif.images.original.url;
+      const carouselItemEl =
+        i === 0
+          ? $(`<div class="carousel-item active">`)
+          : $(`<div class="carousel-item">`);
+      const img = $("<img>");
+      img.attr({ src: gifUrl, class: "d-block w-100", alt: "gif" });
+      carouselItemEl.append(img);
+      $(".carousel-inner").append(carouselItemEl);
+    });
+  });
+});
