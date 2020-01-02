b94ff14d5ef2d48c40c39fc54e3718d6e516ca00
Elrey Belmonti <elbelmonti@gmail.com>
2020-01-01 17:13:16 -0800

    add bootstrap, jquery, moment.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// var queryURL =
+// "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=chuck%norris";
+
+// $.ajax({
+// url: queryURL,
+// method: "GET"
+// }).then(function(response) {
+// var random = Math.floor(Math.random() * response.data.length) + 1;
+// console.log(response.data[random].images.original.url);
+// const gif = response.data[random].images.original.url;
+// const img = $("<img>");
+// img.attr("src", gif);
+// $("#gifs").append(img);
+// });
